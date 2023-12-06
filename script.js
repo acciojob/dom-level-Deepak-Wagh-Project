c179cf57556b4606e992a407c874955f49cb4945
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-06 12:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getLevel(ele,num){
+   if(!ele.parentNode){
+    return num;
+   } 
+
+   return getLevel(ele.parentNode,num+1)
+   
+
+
+}
+const eleToFind=document.getElementById("level");
+alert(getLevel(eleToFind,0));
